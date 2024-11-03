4d249728510690b4cbf49db72697687af1b6819a
cancan <cancanchihao@gmail.com>
2024-11-03 21:46:18 +0800

    微调

--- src/components/chart_1.vue
+++ src/components/chart_1.vue
@@ -12,19 +12,22 @@ const components = {
 const players = ref([
   { name: 'Player 1', stats: [80, 70, 90, 85, 75] },
   { name: 'Player 2', stats: [70, 80, 60, 90, 95] },
-  { name: 'Player 3', stats: [90, 85, 80, 75, 70] },
+  { name: 'Player 3', stats: [90, 85, 80, 70, 70] },
+  { name: 'Player 4', stats: [60, 90, 50, 65, 65] },
+  { name: 'Player 5', stats: [50, 60, 70, 95, 35] },
+  { name: 'Player 6', stats: [40, 65, 100, 80, 75] },
 ]);
 
-// 选中的玩家
-const selectedPlayer = ref(players.value[0]);
+// 选中的组
+const selectedGroup = ref([players.value[0], players.value[1]]); // 默认选中第一组
 
 // ECharts配置
 const chartOptions = ref({
   title: {
-    text: selectedPlayer.value.name,
+    text: '位置1', // 默认标题
   },
   textStyle: {
-      fontSize: 10
+      fontSize: 10,
   },
   tooltip: {},
   radar: {
@@ -40,29 +43,46 @@ const chartOptions = ref({
     {
       name: 'Player Stats',
       type: 'radar',
-      data: [
-        {
-          value: selectedPlayer.value.stats,
-          name: selectedPlayer.value.name,
-        },
-      ],
+      data: selectedGroup.value.map(player => ({
+        value: player.stats,
+        name: player.name,
+      })),
     },
   ],
 });
 
-// 监听选中玩家的变化，更新ECharts配置
-watch(selectedPlayer, (newPlayer) => {
-  chartOptions.value.title.text = newPlayer.name;
-  chartOptions.value.series[0].data[0].value = newPlayer.stats;
-  // 这里直接调用更新
+// 监听选中组的变化，更新ECharts配置
+watch(selectedGroup, (newGroup) => {
+  let groupIndex;
+  if (newGroup[0] === players.value[0] && newGroup[1] === players.value[1]) {
+    groupIndex = 1;
+  } else if (newGroup[0] === players.value[2] && newGroup[1] === players.value[3]) {
+    groupIndex = 2;
+  } else {
+    groupIndex = 3;
+  }
+  chartOptions.value.title.text = `位置${groupIndex}`; // 更新标题为位置1、2或3
+  chartOptions.value.series[0].data = newGroup.map(player => ({
+    value: player.stats,
+    name: player.name,
+  }));
   if (chartInstance) {
     chartInstance.setOption(chartOptions.value); // 更新图表
   }
 });
 
-// 选择玩家的方法
-const selectPlayer = (player) => {
-  selectedPlayer.value = player;
+// 选择组的方法
+const selectGroup = (group) => {
+  selectedGroup.value = group;
+  let groupIndex;
+  if (group[0] === players.value[0] && group[1] === players.value[1]) {
+    groupIndex = 1;
+  } else if (group[0] === players.value[2] && group[1] === players.value[3]) {
+    groupIndex = 2;
+  } else {
+    groupIndex = 3;
+  }
+  chartOptions.value.title.text = `位置${groupIndex}`; // 更新标题为位置1、2或3
 };
 
 let chartInstance;
@@ -88,22 +108,21 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div>
-        <div class="player-list">
-        <ul>
-            <li v-for="player in players" :key="player.name" @click="selectPlayer(player)">
-            {{ player.name }}
-            </li>
-        </ul>
-        </div>
-        <div class="radar-chart" id="radar-chart">
-        <v-chart :options="chartOptions" :auto-resize="true" />
-        </div>
+  <div>
+    <div class="player-list">
+      <ul>
+        <li @click="selectGroup([players[0], players[1]])">位置1</li>
+        <li @click="selectGroup([players[2], players[3]])">位置2</li>
+        <li @click="selectGroup([players[4], players[5]])">位置3</li>
+      </ul>
     </div>
+    <div class="radar-chart" id="radar-chart">
+      <v-chart :options="chartOptions" :auto-resize="true" />
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
-
 .player-list {
   width: 90px;
   font-size: 12px;
@@ -129,7 +148,6 @@ onUnmounted(() => {
 }
 
 .radar-chart {
-  z-index: 1000;
   padding-top: 10px;
   width: 270px;
   height: 200px;
--- src/components/chart_temp.vue
+++ src/components/chart_temp.vue
@@ -1,5 +1,94 @@
-<!-- 临时文件存放，该文件没有用处！
-可以当剪切板用 -->
+<script setup>
+import { ref, watch, onMounted, onUnmounted } from 'vue';
+import * as echarts from 'echarts';
+import VChart from 'vue-echarts';
+
+// 注册vchart组件
+const components = {
+  VChart,
+};
+
+// 玩家数据
+const players = ref([
+  { name: 'Player 1', stats: [80, 70, 90, 85, 75] },
+  { name: 'Player 2', stats: [70, 80, 60, 90, 95] },
+  { name: 'Player 3', stats: [90, 85, 80, 70, 70] },
+  { name: 'Player 4', stats: [60, 90, 50, 65, 65] },
+  { name: 'Player 5', stats: [50, 60, 70, 95, 35] },
+  { name: 'Player 6', stats: [40, 65, 100, 80, 75] },
+]);
+
+// 选中的玩家
+const selectedPlayer = ref(players.value[0]);
+
+// ECharts配置
+const chartOptions = ref({
+  title: {
+    text: selectedPlayer.value.name,
+  },
+  textStyle: {
+      fontSize: 10
+  },
+  tooltip: {},
+  radar: {
+    indicator: [
+      { name: 'Attack', max: 100 },
+      { name: 'Defense', max: 100 },
+      { name: 'Skill', max: 100 },
+      { name: 'Strategy', max: 100 },
+      { name: 'Teamwork', max: 100 },
+    ],
+  },
+  series: [
+    {
+      name: 'Player Stats',
+      type: 'radar',
+      data: [
+        {
+          value: selectedPlayer.value.stats,
+          name: selectedPlayer.value.name,
+        },
+      ],
+    },
+  ],
+});
+
+// 监听选中玩家的变化，更新ECharts配置
+watch(selectedPlayer, (newPlayer) => {
+  chartOptions.value.title.text = newPlayer.name;
+  chartOptions.value.series[0].data[0].value = newPlayer.stats;
+  // 这里直接调用更新
+  if (chartInstance) {
+    chartInstance.setOption(chartOptions.value); // 更新图表
+  }
+});
+
+// 选择玩家的方法
+const selectPlayer = (player) => {
+  selectedPlayer.value = player;
+};
+
+let chartInstance;
+
+onMounted(() => {
+  const chartDom = document.getElementById('radar-chart');
+  chartInstance = echarts.init(chartDom);
+  chartInstance.setOption(chartOptions.value);
+
+  // 监听窗口大小变化
+  window.addEventListener('resize', () => {
+    chartInstance.resize();
+  });
+});
+
+onUnmounted(() => {
+  if (chartInstance) {
+    chartInstance.dispose();
+  }
+  window.removeEventListener('resize', chartInstance.resize);
+});
+
+</script>
 
 <template>
     <div>
@@ -10,91 +99,44 @@
             </li>
         </ul>
         </div>
-        <div class="radar-chart">
-        <v-chart :options="chartOptions" />
+        <div class="radar-chart" id="radar-chart">
+        <v-chart :options="chartOptions" :auto-resize="true" />
         </div>
     </div>
-    </template>
-    
-    <script>
-    import * as echarts from 'echarts';
-    import { defineComponent, ref } from 'vue';
-    import { ECharts } from 'vue-echarts';
-    import { use } from 'echarts/core';
-    import {
-    RadarChart,
-    TitleComponent,
-    TooltipComponent,
-    GridComponent,
-    } from 'echarts/components';
-    
-    use([RadarChart, TitleComponent, TooltipComponent, GridComponent]);
-    
-    export default defineComponent({
-    components: {
-        VChart: ECharts,
-    },
-    setup() {
-        const players = ref([
-        { name: 'Player 1', stats: [80, 70, 90, 85, 75] },
-        { name: 'Player 2', stats: [70, 80, 60, 90, 95] },
-        { name: 'Player 3', stats: [90, 85, 80, 75, 70] },
-        ]);
-    
-        const selectedPlayer = ref(players.value[0]);
-    
-        const chartOptions = ref({
-        title: {
-            text: selectedPlayer.value.name,
-        },
-        tooltip: {},
-        radar: {
-            indicator: [
-            { name: 'Attack', max: 100 },
-            { name: 'Defense', max: 100 },
-            { name: 'Skill', max: 100 },
-            { name: 'Strategy', max: 100 },
-            { name: 'Teamwork', max: 100 },
-            ],
-        },
-        series: [
-            {
-            name: 'Player Stats',
-            type: 'radar',
-            data: [
-                {
-                value: selectedPlayer.value.stats,
-                name: selectedPlayer.value.name,
-                },
-            ],
-            },
-        ],
-        });
-    
-        const selectPlayer = (player) => {
-        selectedPlayer.value = player;
-        chartOptions.value.title.text = player.name;
-        chartOptions.value.series[0].data[0].value = player.stats;
-        };
-    
-        return {
-        players,
-        chartOptions,
-        selectPlayer,
-        };
-    },
-    });
-    </script>
-    
-    <style>
-    .player-list {
-    float: left;
-    width: 200px;
+</template>
+
+<style lang="less" scoped>
+
+.player-list {
+  width: 90px;
+  font-size: 12px;
+  display: inline-block;
+  vertical-align: top;
+  height: 180px;
+  ul {
+    list-style: none; 
+    padding: 10px; 
+    margin: 0; 
+    li {
+      cursor: pointer;
+      border-bottom: 1px solid blue;
+      padding: 3px;
+      margin-bottom: 5px;
+      transition: font-size 0.3s ease, color 0.3s ease;
     }
-    
-    .radar-chart {
-    float: right;
-    width: 400px;
+    li:hover {
+      font-size: 16px;
+      color: rgb(73, 11, 217);
     }
-    </style>
-    
+  }
+}
+
+.radar-chart {
+  z-index: 1000;
+  padding-top: 10px;
+  width: 270px;
+  height: 200px;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
