1145ee13b918c90f847ccc0d6c536311fc2dd232
cancan <cancanchihao@gmail.com>
2024-11-05 20:31:38 +0800

    微调

--- src/components/chart_temp.vue
+++ src/components/chart_temp.vue
@@ -15,19 +15,26 @@ const players = ref([
   { name: 'Player 3', stats: [90, 85, 80, 70, 70] },
   { name: 'Player 4', stats: [60, 90, 50, 65, 65] },
   { name: 'Player 5', stats: [50, 60, 70, 95, 35] },
-  { name: 'Player 6', stats: [40, 65, 100, 80, 75] },
+  { name: 'Player 6', stats: [40, 65, 99, 80, 75] },
 ]);
 
-// 选中的玩家
-const selectedPlayer = ref(players.value[0]);
+// 玩家组
+const groups = ref([
+  [players.value[0], players.value[1]], // 组 1
+  [players.value[2], players.value[3]], // 组 2
+  [players.value[4], players.value[5]], // 组 3
+]);
 
-// ECharts配置
-const chartOptions = ref({
+// 选中的组
+const selectedGroup = ref(groups.value[0]); // 默认选中第一组
+
+// ECharts配置生成函数
+const createChartOptions = (player) => ({
   title: {
-    text: selectedPlayer.value.name,
+    text: player.name, // 标题为玩家名称
   },
   textStyle: {
-      fontSize: 10
+    fontSize: 10,
   },
   tooltip: {},
   radar: {
@@ -41,83 +48,94 @@ const chartOptions = ref({
   },
   series: [
     {
-      name: 'Player Stats',
+      name: `${player.name} Stats`,
       type: 'radar',
-      data: [
-        {
-          value: selectedPlayer.value.stats,
-          name: selectedPlayer.value.name,
-        },
-      ],
+      data: [{
+        value: player.stats,
+        name: player.name,
+      }],
     },
   ],
 });
 
-// 监听选中玩家的变化，更新ECharts配置
-watch(selectedPlayer, (newPlayer) => {
-  chartOptions.value.title.text = newPlayer.name;
-  chartOptions.value.series[0].data[0].value = newPlayer.stats;
-  // 这里直接调用更新
-  if (chartInstance) {
-    chartInstance.setOption(chartOptions.value); // 更新图表
+// 监听选中组变化，更新图表
+watch(selectedGroup, () => {
+  if (chartInstanceLeft) {
+    chartInstanceLeft.setOption(createChartOptions(selectedGroup.value[0])); // 更新左侧图表
+  }
+  if (chartInstanceRight) {
+    chartInstanceRight.setOption(createChartOptions(selectedGroup.value[1])); // 更新右侧第一个图表
   }
 });
 
-// 选择玩家的方法
-const selectPlayer = (player) => {
-  selectedPlayer.value = player;
+// 选择组的方法
+const selectGroup = (group) => {
+  selectedGroup.value = group;
 };
 
-let chartInstance;
+let chartInstanceLeft;
+let chartInstanceRight;
 
 onMounted(() => {
-  const chartDom = document.getElementById('radar-chart');
-  chartInstance = echarts.init(chartDom);
-  chartInstance.setOption(chartOptions.value);
+  // 初始化左侧图表
+  const chartDomLeft = document.getElementById('radar-chart-left');
+  chartInstanceLeft = echarts.init(chartDomLeft);
+  chartInstanceLeft.setOption(createChartOptions(selectedGroup.value[0]));
+
+  // 初始化右侧第一个图表
+  const chartDomRight = document.getElementById('radar-chart-right');
+  chartInstanceRight = echarts.init(chartDomRight);
+  chartInstanceRight.setOption(createChartOptions(selectedGroup.value[1]));
 
   // 监听窗口大小变化
   window.addEventListener('resize', () => {
-    chartInstance.resize();
+    chartInstanceLeft.resize();
+    chartInstanceRight.resize();
   });
 });
 
 onUnmounted(() => {
-  if (chartInstance) {
-    chartInstance.dispose();
+  if (chartInstanceLeft) {
+    chartInstanceLeft.dispose();
   }
-  window.removeEventListener('resize', chartInstance.resize);
+  if (chartInstanceRight) {
+    chartInstanceRight.dispose();
+  }
+  window.removeEventListener('resize', chartInstanceLeft.resize);
+  window.removeEventListener('resize', chartInstanceRight.resize);
 });
-
 </script>
 
 <template>
-    <div>
-        <div class="player-list">
-        <ul>
-            <li v-for="player in players" :key="player.name" @click="selectPlayer(player)">
-            {{ player.name }}
-            </li>
-        </ul>
-        </div>
-        <div class="radar-chart" id="radar-chart">
-        <v-chart :options="chartOptions" :auto-resize="true" />
-        </div>
+  <div>
+    <div class="player-list">
+      <ul>
+        <li @click="selectGroup(groups[0])">组 1: Player 1 和 Player 2</li>
+        <li @click="selectGroup(groups[1])">组 2: Player 3 和 Player 4</li>
+        <li @click="selectGroup(groups[2])">组 3: Player 5 和 Player 6</li>
+      </ul>
     </div>
+    <div class="radar-container">
+      <!-- 左侧雷达图 -->
+      <div class="radar-chart" id="radar-chart-left"></div>
+      <!-- 右侧雷达图 -->
+      <div class="radar-chart" id="radar-chart-right"></div>
+    </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
-
 .player-list {
-  width: 90px;
+  width: 150px;
   font-size: 12px;
   display: inline-block;
   vertical-align: top;
-  height: 180px;
   ul {
-    list-style: none; 
-    padding: 10px; 
-    margin: 0; 
+    list-style: none;
+    padding: 10px;
+    margin: 0;
     li {
+      font-size: 16px;
       cursor: pointer;
       border-bottom: 1px solid blue;
       padding: 3px;
@@ -125,18 +143,21 @@ onUnmounted(() => {
       transition: font-size 0.3s ease, color 0.3s ease;
     }
     li:hover {
-      font-size: 16px;
+      font-size: 20px;
       color: rgb(73, 11, 217);
     }
   }
 }
 
+.radar-container {
+  display: flex;
+  // justify-content: space-between;
+}
+
 .radar-chart {
-  z-index: 1000;
+  width: 300px;
   padding-top: 10px;
-  width: 270px;
-  height: 200px;
-  display: inline-block;
-  vertical-align: top;
+  width: 30%;
+  height: 300px;
 }
 </style>
