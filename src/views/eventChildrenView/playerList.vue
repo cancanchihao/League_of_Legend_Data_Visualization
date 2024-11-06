baad01c6a081f508206522c854bb93299719b011
zzy <1808649570@qq.com>
2024-11-06 20:58:32 +0800

    .

--- src/views/eventChildrenView/playerList.vue
+++ src/views/eventChildrenView/playerList.vue
@@ -1,10 +1,10 @@
 <script setup>
-import Chart1 from '@/components/chart_1.vue';
+import Chart2 from '@/components/chart_2.vue';
 
 </script>
 
 <template>
-    <Chart1/>
+    <Chart2 />
 </template>
 
 <style scoped></style>
