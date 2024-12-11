36b3a568041f78f0984bf63b43e788ce2d0b3a06
zzy <1808649570@qq.com>
2024-12-11 16:39:19 +0800

    123

--- src/components/hero-against-chart.vue
+++ src/components/hero-against-chart.vue
@@ -1,37 +1,28 @@
 <template>
     <div class="hero">
-        <img :src="'/src/assets/'+props.heroData[0].headimg+'.png'" alt="1">
+        <!-- <img :src="'/src/assets/' + props.heroData[0].headimg + '.png'" alt="1"> -->
+        <v-avatar class="avatar1" color="grey-darken-1" size="48" style="cursor: pointer;">
+            <v-img :src="'http://192.168.198.10:8080/hero/heroImg?heroName=' + '暗裔剑魔'"></v-img>
+        </v-avatar>
         <el-button type="primary" class="elb" @click="change(1)">
             {{ props.heroData[0].name }}
         </el-button>
         <el-button type="primary" class="elb" @click="change(2)">
             {{ props.heroData[1].name }}
         </el-button>
-        <img :src="'/src/assets/'+props.heroData[1].headimg+'.png'" alt="1">
+        <!-- <img :src="'/src/assets/' + props.heroData[1].headimg + '.png'" alt="1"> -->
+        <v-avatar class="avatar1" color="grey-darken-1" size="48" style="cursor: pointer;">
+            <v-img :src="'http://192.168.198.10:8080/hero/heroImg?heroName=' + '暗裔剑魔'"></v-img>
+        </v-avatar>
     </div>
-    <item
-        :line_name='"  胜率 "'
-        :data1="props.heroData[0].winRate"
-        :data2="props.heroData[1].winRate"
-        :obj1="props.heroData[0].name"
-        :obj2="props.heroData[1].name"
-        :selectedItem="selectedItem">
+    <item :line_name='"  胜率 "' :data1="props.heroData[0].winRate" :data2="props.heroData[1].winRate"
+        :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem">
     </item>
-    <item 
-        :line_name='"pick率"' 
-        :data1="props.heroData[0].pickRate" 
-        :data2="props.heroData[1].pickRate" 
-        :obj1="props.heroData[0].name" 
-        :obj2="props.heroData[1].name"
-        :selectedItem="selectedItem">
+    <item :line_name='"pick率"' :data1="props.heroData[0].pickRate" :data2="props.heroData[1].pickRate"
+        :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem">
     </item>
-    <item 
-        :line_name='"ban率"' 
-        :data1="props.heroData[0].banRate" 
-        :data2="props.heroData[1].banRate" 
-        :obj1="props.heroData[0].name" 
-        :obj2="props.heroData[1].name"
-        :selectedItem="selectedItem">
+    <item :line_name='"ban率"' :data1="props.heroData[0].banRate" :data2="props.heroData[1].banRate"
+        :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem">
     </item>
 </template>
 
@@ -45,8 +36,8 @@ import item from './against-chart-item.vue';
 
 const props = defineProps({
     heroData: {
-      type: Array,
-      required: true,
+        type: Array,
+        required: true,
     },
 });
 
@@ -75,7 +66,9 @@ const change = (id) => {
 .elb {
     display: inline-flex;
     align-items: center;
-    position: relative; /* 使 z-index 生效 */
-    z-index: 10; /* 设置更高的 z-index */
+    position: relative;
+    /* 使 z-index 生效 */
+    z-index: 10;
+    /* 设置更高的 z-index */
 }
 </style>
