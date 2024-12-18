d8ee79e26eb78523157a8e3185613d3ace8ecc03
zzy <1808649570@qq.com>
2024-12-18 13:57:00 +0800

    123

--- src/components/team-against-chart.vue
+++ src/components/team-against-chart.vue
@@ -1,92 +1,43 @@
 <template>
   <div class="team">
-    <v-avatar
-      class="avatar1"
-      color="grey-darken-1"
-      size="48"
-      style="cursor: pointer"
-    >
-      <v-img
-        :src="
-          'http://1.14.45.200:8080/team/teamImg?teamName=' +
-          props.teamData[0].name +
-          '&&matchType=' +
-          props.matchType
-        "
-      ></v-img>
-    </v-avatar>
+
+
+    <v-img :src="'http://1.14.45.200:8080/team/teamImg?teamName=' +
+      props.teamData[0].name +
+      '&&matchType=' +
+      props.matchType
+      " height="7vh" width="3vw">
+    </v-img>
     <el-button type="primary" class="elb button1" @click="change(1)">
       {{ props.teamData[0].name }}
     </el-button>
     <el-button type="warning" class="elb button2" @click="change(2)">
       {{ props.teamData[1].name }}
     </el-button>
-    <v-avatar
-      class="avatar1"
-      color="grey-darken-1"
-      size="48"
-      style="cursor: pointer"
-    >
-      <v-img
-        :src="
-          'http://1.14.45.200:8080/team/teamImg?teamName=' +
-          props.teamData[1].name +
-          '&&matchType=' +
-          props.matchType
-        "
-      ></v-img>
-    </v-avatar>
+    <v-img :src="'http://1.14.45.200:8080/team/teamImg?teamName=' +
+      props.teamData[1].name +
+      '&&matchType=' +
+      props.matchType
+      " height=" 7vh" width="3vw">
+    </v-img>
+
+
   </div>
-  <item
-    :line_name="'历史比分'"
-    :data1="props.teamData[0].winCount"
-    :data2="props.teamData[1].winCount"
-    :obj1="props.teamData[0].name"
-    :obj2="props.teamData[1].name"
-    :selectedItem="selectedItem"
-    :color1="'#95e1d3'"
-    :color2="'#eaffd0'"
-  ></item>
-  <item
-    :line_name="'   KDA   '"
-    :data1="props.teamData[0].kda"
-    :data2="props.teamData[1].kda"
-    :obj1="props.teamData[0].name"
-    :obj2="props.teamData[1].name"
-    :selectedItem="selectedItem"
-    :color1="'#95e1d3'"
-    :color2="'#eaffd0'"
-  ></item>
-  <item
-    :line_name="'场均大龙'"
-    :data1="props.teamData[0].baron"
-    :data2="props.teamData[1].baron"
-    :obj1="props.teamData[0].name"
-    :obj2="props.teamData[1].name"
-    :selectedItem="selectedItem"
-    :color1="'#95e1d3'"
-    :color2="'#eaffd0'"
-  ></item>
-  <item
-    :line_name="'场均小龙'"
-    :data1="props.teamData[0].dragon"
-    :data2="props.teamData[1].dragon"
-    :obj1="props.teamData[0].name"
-    :obj2="props.teamData[1].name"
-    :selectedItem="selectedItem"
-    :color1="'#95e1d3'"
-    :color2="'#eaffd0'"
-  ></item>
-  <item
-    :line_name="'场均推塔'"
-    :data1="props.teamData[0].turts"
-    :data2="props.teamData[1].turts"
-    :obj1="props.teamData[0].name"
-    :obj2="props.teamData[1].name"
-    :selectedItem="selectedItem"
-    :color1="'#95e1d3'"
-    :color2="'#eaffd0'"
-  ></item>
+  <item :line_name="'历史比分'" :data1="props.teamData[0].winCount" :data2="props.teamData[1].winCount"
+    :obj1="props.teamData[0].name" :obj2="props.teamData[1].name" :selectedItem="selectedItem" :color1="'#95e1d3'"
+    :color2="'#eaffd0'"></item>
+  <item :line_name="'   KDA   '" :data1="props.teamData[0].kda" :data2="props.teamData[1].kda"
+    :obj1="props.teamData[0].name" :obj2="props.teamData[1].name" :selectedItem="selectedItem" :color1="'#95e1d3'"
+    :color2="'#eaffd0'"></item>
+  <item :line_name="'场均大龙'" :data1="props.teamData[0].baron" :data2="props.teamData[1].baron"
+    :obj1="props.teamData[0].name" :obj2="props.teamData[1].name" :selectedItem="selectedItem" :color1="'#95e1d3'"
+    :color2="'#eaffd0'"></item>
+  <item :line_name="'场均小龙'" :data1="props.teamData[0].dragon" :data2="props.teamData[1].dragon"
+    :obj1="props.teamData[0].name" :obj2="props.teamData[1].name" :selectedItem="selectedItem" :color1="'#95e1d3'"
+    :color2="'#eaffd0'"></item>
+  <item :line_name="'场均推塔'" :data1="props.teamData[0].turts" :data2="props.teamData[1].turts"
+    :obj1="props.teamData[0].name" :obj2="props.teamData[1].name" :selectedItem="selectedItem" :color1="'#95e1d3'"
+    :color2="'#eaffd0'"></item>
 </template>
 
 <script setup>
@@ -136,12 +87,15 @@ const change = (id) => {
 }
 
 .button1 {
-  background-color: #95e1d3; /* 设置第一个按钮的背景色 */
+  background-color: #95e1d3;
+  /* 设置第一个按钮的背景色 */
   font-size: 18px;
+  margin-right: 5vw;
 }
 
 .button2 {
-  background-color: #eaffd0; /* 设置第二个按钮的背景色 */
+  background-color: #eaffd0;
+  /* 设置第二个按钮的背景色 */
   font-size: 18px;
 }
 </style>
