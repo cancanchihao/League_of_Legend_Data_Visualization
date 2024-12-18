73923a9cdd358a447c9b7a1f4275f401273ea3ec
Phosgene <1544982718@qq.com>
2024-12-18 15:29:46 +0800

    1

--- src/components/hero-against-chart.vue
+++ src/components/hero-against-chart.vue
@@ -1,49 +1,106 @@
 <template>
-    <div class="hero">
-        <v-avatar class="avatar1" color="grey-darken-1" size="48" style="cursor: pointer;">
-            <v-img :src="'http://192.168.198.10:8080/hero/heroImg?heroName=' + props.heroData[0].name"
-                :alt=props.heroData[0].name></v-img>
-        </v-avatar>
-        <el-button type="success" class="elb button1" @click="change(1)">
-            {{ props.heroData[0].name }}
-        </el-button>
-        <el-button type="info" class="elb button2" @click="change(2)">
-            {{ props.heroData[1].name }}
-        </el-button>
-        <v-avatar class="avatar1" color="grey-darken-1" size="48" style="cursor: pointer;">
-            <v-img :src="'http://192.168.198.10:8080/hero/heroImg?heroName=' + props.heroData[1].name"
-                :alt=props.heroData[1].name></v-img>
-        </v-avatar>
-    </div>
+  <div class="hero">
+    <v-avatar
+      class="avatar1"
+      color="grey-darken-1"
+      size="48"
+      style="cursor: pointer"
+    >
+      <v-img
+        :src="
+          'http://1.14.45.200:8080/hero/heroImg?heroName=' +
+          props.heroData[0].name
+        "
+        :alt="props.heroData[0].name"
+      ></v-img>
+    </v-avatar>
+    <el-button type="success" class="elb button1" @click="change(1)">
+      {{ props.heroData[0].name }}
+    </el-button>
+    <el-button type="info" class="elb button2" @click="change(2)">
+      {{ props.heroData[1].name }}
+    </el-button>
+    <v-avatar
+      class="avatar1"
+      color="grey-darken-1"
+      size="48"
+      style="cursor: pointer"
+    >
+      <v-img
+        :src="
+          'http://1.14.45.200:8080/hero/heroImg?heroName=' +
+          props.heroData[1].name
+        "
+        :alt="props.heroData[1].name"
+      ></v-img>
+    </v-avatar>
+  </div>
 
-    <!-- 当两个 winCount 都为 0 时，显示提示信息 -->
-    <div v-if="props.heroData[0].winCount === 0 && props.heroData[1].winCount === 0" class="no-matches">
-        该英雄间无对位情况
-    </div>
+  <!-- 当两个 winCount 都为 0 时，显示提示信息 -->
+  <div
+    v-if="props.heroData[0].winCount === 0 && props.heroData[1].winCount === 0"
+    class="no-matches"
+  >
+    该英雄间无对位情况
+  </div>
 
-    <div v-else>
-        <item :line_name='"对位胜场"' :data1="props.heroData[0].winCount" :data2="props.heroData[1].winCount"
-            :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem" 
-            :color1="'#fce38a'" :color2="'#f38181'">
-        </item>
-        <item :line_name='"   KDA   "' :data1="props.heroData[0].kda" :data2="props.heroData[1].kda"
-            :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem" 
-            :color1="'#fce38a'" :color2="'#f38181'">
-        </item>
-        <item :line_name='"场均经济"' :data1="props.heroData[0].gold" :data2="props.heroData[1].gold"
-            :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem" 
-            :color1="'#fce38a'" :color2="'#f38181'">
-        </item>
-        <item :line_name='"场均伤害"' :data1="props.heroData[0].damage" :data2="props.heroData[1].damage"
-            :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem" 
-            :color1="'#fce38a'" :color2="'#f38181'">
-        </item>
-        <item :line_name='"场均承伤"' :data1="props.heroData[0].tanking" :data2="props.heroData[1].tanking"
-            :obj1="props.heroData[0].name" :obj2="props.heroData[1].name" :selectedItem="selectedItem" 
-            :color1="'#fce38a'" :color2="'#f38181'">
-        </item>
-    </div>
-    
+  <div v-else>
+    <item
+      :line_name="'对位胜场'"
+      :data1="props.heroData[0].winCount"
+      :data2="props.heroData[1].winCount"
+      :obj1="props.heroData[0].name"
+      :obj2="props.heroData[1].name"
+      :selectedItem="selectedItem"
+      :color1="'#fce38a'"
+      :color2="'#f38181'"
+    >
+    </item>
+    <item
+      :line_name="'   KDA   '"
+      :data1="props.heroData[0].kda"
+      :data2="props.heroData[1].kda"
+      :obj1="props.heroData[0].name"
+      :obj2="props.heroData[1].name"
+      :selectedItem="selectedItem"
+      :color1="'#fce38a'"
+      :color2="'#f38181'"
+    >
+    </item>
+    <item
+      :line_name="'场均经济'"
+      :data1="props.heroData[0].gold"
+      :data2="props.heroData[1].gold"
+      :obj1="props.heroData[0].name"
+      :obj2="props.heroData[1].name"
+      :selectedItem="selectedItem"
+      :color1="'#fce38a'"
+      :color2="'#f38181'"
+    >
+    </item>
+    <item
+      :line_name="'场均伤害'"
+      :data1="props.heroData[0].damage"
+      :data2="props.heroData[1].damage"
+      :obj1="props.heroData[0].name"
+      :obj2="props.heroData[1].name"
+      :selectedItem="selectedItem"
+      :color1="'#fce38a'"
+      :color2="'#f38181'"
+    >
+    </item>
+    <item
+      :line_name="'场均承伤'"
+      :data1="props.heroData[0].tanking"
+      :data2="props.heroData[1].tanking"
+      :obj1="props.heroData[0].name"
+      :obj2="props.heroData[1].name"
+      :selectedItem="selectedItem"
+      :color1="'#fce38a'"
+      :color2="'#f38181'"
+    >
+    </item>
+  </div>
 </template>
 
 <script setup>
